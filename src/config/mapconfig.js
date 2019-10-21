5ffd532cc4a54179963407957f140fc5e7a2d620
Lenvo <gis_rc@163.com>
2019-10-21 16:20:49 +0800

    'addzss'

--- src/config/mapconfig.js
+++ src/config/mapconfig.js
@@ -123,6 +123,6 @@ let  drawKriging=function (extent) {
     }
 };
 //首次加载，自动渲染一次插值图
-let extent1=clipgeom.getExtent();
-drawKriging(extent1);
+let extent=clipgeom.getExtent();
+drawKriging(extent);
 export default mapconfig;
